69fdf21f5212100894780563061694bf417b2f83
daoleno <daodaoonline@gmail.com>
2020-03-28 09:23:08 +0800

    Init tombstone module

---
+++ x/tombstone/alias.go
@@ -0,0 +1,40 @@
+package tombstone
+
+import (
+	"github.com/daoleno/higan/x/tombstone/internal/keeper"
+	"github.com/daoleno/higan/x/tombstone/internal/types"
+)
+
+const (
+	// TODO: define constants that you would like exposed from the internal package
+
+	ModuleName        = types.ModuleName
+	RouterKey         = types.RouterKey
+	StoreKey          = types.StoreKey
+	DefaultParamspace = types.DefaultParamspace
+	QueryParams       = types.QueryParams
+	QuerierRoute      = types.QuerierRoute
+)
+
+var (
+	// functions aliases
+	NewKeeper           = keeper.NewKeeper
+	NewQuerier          = keeper.NewQuerier
+	RegisterCodec       = types.RegisterCodec
+	NewGenesisState     = types.NewGenesisState
+	DefaultGenesisState = types.DefaultGenesisState
+	ValidateGenesis     = types.ValidateGenesis
+	// TODO: Fill out function aliases
+
+	// variable aliases
+	ModuleCdc     = types.ModuleCdc
+	// TODO: Fill out variable aliases
+)
+
+type (
+	Keeper       = keeper.Keeper
+	GenesisState = types.GenesisState
+	Params       = types.Params
+
+	// TODO: Fill out module types
+)
